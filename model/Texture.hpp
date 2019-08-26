335ef93e66d10d9388cf424493f4bfad0915915b
Alexander Kraus <nr4@z10.info>
2019-08-26 12:07:32 +0200

    added render graph model classes

---
+++ model/Texture.hpp
@@ -0,0 +1,34 @@
+/* cuddly-eureka - Storyboard editor by Team210
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "Framebuffer.hpp"
+
+class Texture : public Framebuffer
+{
+public:
+    Texture(int width, int height);
+    virtual ~Texture();
+    
+private:
+    int m_width,
+        m_height;
+};
+
+#endif
